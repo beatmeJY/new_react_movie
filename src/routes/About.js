f1cbab8e5eb24c42ca0fbe41339facfb6fc1a2b7
Choi JiYoul <wldbf97@naver.com>
2020-07-02 17:39:45 +0900

    edit Finish!!

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>
+        About this page: ok?
+      </span>
+      <span>- george</span>
+    </div>
+  )
+}
+
+export default About;
